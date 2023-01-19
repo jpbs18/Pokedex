07b5842092143856b14ecdb142499c6444cd3302
João Santos <joao.santos@mindswap.academy>
2023-01-19 10:43:04 +0000

    Respositivity finished

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,15 @@
 import './app.css';
 import {MyHeader, MyMain, MyAside,MyFooter} from "../../layout"
+import {useWindowDimensions} from "../../hooks/useWindowDimensions";
 
 export default () => {
+
+    const {height, width} = useWindowDimensions()
+
     return (
         <div className="App-Container">
             <MyHeader/>
-            <MyAside/>
+            {width < 420 || <MyAside/>}
             <MyMain/>
             <MyFooter/>
         </div>
